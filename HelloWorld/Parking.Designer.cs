0136e9edfaadee6dd26f132a90095a954ab228f7
zain118 <zainul118@gmail.com>
2019-01-29 16:37:24 +0530

    Smart Parking Commit

---
+++ HelloWorld/Parking.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HelloWorld
+{
+    partial class Parking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parkingLive = new System.Windows.Forms.PictureBox();
+            this.cameraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lblParkingStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.parkingLive)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // parkingLive
+            // 
+            this.parkingLive.Location = new System.Drawing.Point(12, 42);
+            this.parkingLive.Name = "parkingLive";
+            this.parkingLive.Size = new System.Drawing.Size(675, 424);
+            this.parkingLive.TabIndex = 0;
+            this.parkingLive.TabStop = false;
+            this.parkingLive.Click += new System.EventHandler(this.Capture_ImageGrabbed);
+            // 
+            // cameraToolStripMenuItem
+            // 
+            this.cameraToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cameraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.pauseToolStripMenuItem});
+            this.cameraToolStripMenuItem.Name = "cameraToolStripMenuItem";
+            this.cameraToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.cameraToolStripMenuItem.Text = "Camera";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            this.pauseToolStripMenuItem.Click += new System.EventHandler(this.pauseToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cameraToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1245, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lblParkingStatus
+            // 
+            this.lblParkingStatus.AutoSize = true;
+            this.lblParkingStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParkingStatus.Location = new System.Drawing.Point(722, 42);
+            this.lblParkingStatus.Name = "lblParkingStatus";
+            this.lblParkingStatus.Size = new System.Drawing.Size(70, 25);
+            this.lblParkingStatus.TabIndex = 2;
+            this.lblParkingStatus.Text = "label1";
+            // 
+            // Parking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 478);
+            this.Controls.Add(this.lblParkingStatus);
+            this.Controls.Add(this.parkingLive);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Parking";
+            this.Text = "Smart PARKING";
+            ((System.ComponentModel.ISupportInitialize)(this.parkingLive)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox parkingLive;
+        private System.Windows.Forms.ToolStripMenuItem cameraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label lblParkingStatus;
+    }
+}
+
---
+++ HelloWorld/Parking.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
+namespace HelloWorld
+{
+    public partial class Parking : Form
+    {
+        VideoCapture _capture;
+        int MIN_PERCENTAGE_COVERED = 5;
+        // BGR
+        MCvScalar RED = new MCvScalar(0, 0, 255);
+        MCvScalar GREEN = new MCvScalar(0, 255, 0);
+        MCvScalar BLUE = new MCvScalar(255, 0, 0);
+
+        public Parking()
+        {
+            InitializeComponent();
+            lblParkingStatus.Text = string.Empty;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_capture == null) _capture = new Emgu.CV.VideoCapture(0);
+
+                _capture.ImageGrabbed += Capture_ImageGrabbed;
+                _capture.Start();
+            }
+            catch (Exception ex)
+            {
+                HandleError(ex);
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.Stop();
+            lblParkingStatus.Text = string.Empty;
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_capture != null) _capture.Pause();
+            lblParkingStatus.Text = string.Empty;
+        }
+
+        #region [Process Frame & Update output]
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat mat = new Mat();
+                _capture.Retrieve(mat);
+
+                ProcessImage(mat.ToImage<Bgr, Byte>());
+            }
+            catch (Exception ex)
+            {
+                HandleError(ex);
+            }
+        }
+
+        private void ProcessImage(Image<Bgr, byte> input_image)
+        {
+            try
+            {
+                // trick is in gray to identify particular object :-
+                Image<Gray, byte> gray_image = input_image.Convert<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(250));
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat hier = new Mat();
+                CvInvoke.FindContours(gray_image, contours, hier, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
+
+                double total_area_covered = 0.0;
+                double parking_slot_area = 0.0;
+                int parking_contour_index = 2;
+                Dictionary<int, double> listOfContourAreas = new Dictionary<int, double>();
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    var ctr = contours[i];
+                    double contour_area = CvInvoke.ContourArea(ctr);
+
+                    listOfContourAreas.Add(i, contour_area);
+                }
+
+                // sort reverse :-
+                var sorted_dict = from pair in listOfContourAreas
+                                  orderby pair.Value descending
+                                  select pair;
+
+                KeyValuePair<int, double> parking_slot_contour = sorted_dict.ElementAt(1); //second highest area :-
+
+                parking_contour_index = parking_slot_contour.Key;
+                parking_slot_area = parking_slot_contour.Value;
+
+                foreach (KeyValuePair<int, double> ctr in listOfContourAreas)
+                {
+                    if (ctr.Value > 0 && ctr.Value < parking_slot_area)
+                    {
+                        total_area_covered += ctr.Value;
+                        //CvInvoke.DrawContours(input_image, contours, ctr.Key, BLUE, 1, LineType.EightConnected);
+                    }
+                }
+
+                // Calculate percentage of area covered by car :-
+                double percentage_covered = (total_area_covered / parking_slot_area) * 100;
+
+                // MCvScalar - uses (BGR) code - Blue, Green, Red :-
+                if (percentage_covered > MIN_PERCENTAGE_COVERED)
+                {
+                    //CvInvoke.DrawContours(input_image, contours, parking_contour_index, RED, 3, LineType.Filled);
+                    UpdateOutputLabel("Parking FULL !!");
+                }
+                else
+                {
+                    ///CvInvoke.DrawContours(input_image, contours, parking_contour_index, GREEN, 3, LineType.Filled);
+                    UpdateOutputLabel("Parking Vacant !!");
+                }
+
+                // Show the final image :-
+                parkingLive.Image = input_image.Bitmap;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error: " + ex.Message + " - " + ex.StackTrace);
+            }
+        }
+
+        private void UpdateOutputLabel(string parkingStatusMessage)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    lblParkingStatus.Text = parkingStatusMessage;
+                }));
+            }
+            else
+            {
+                lblParkingStatus.Text = parkingStatusMessage;
+            }
+        }
+
+        #endregion
+
+        // Error handling
+        private void HandleError(Exception ex)
+        {
+            if (_capture != null)
+            {
+                _capture.Stop();
+                _capture = null;
+            }
+
+            // MessageBox.Show("Error: " + ex.Message + " - " + ex.StackTrace);
+        }
+
+    }
+}
